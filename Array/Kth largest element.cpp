9a67936749b9babffe5fd802afb6dba8b97568fb
Jyothi Swaroopa Mamidipalli <119839745+JyothiMamidipalli@users.noreply.github.com>
2023-10-08 23:09:01 +0530

    Create Kth largest element.cpp

---
+++ Array/Kth largest element.cpp
@@ -0,0 +1,18 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    //jyo_code();
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int k;
+    cin>>k;
+    sort(arr,arr+n);
+    cout<<arr[k-1];
+    return 0;
+}
